ac7e686e38c6266bd688e0c63337c6ba755fa34e
campbell <campbellrehu@me.com>
2017-06-01 13:39:12 +1200

    Added Search component

---
+++ src/Search.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: ''
+        }
+    }
+    
+    handleSearch(event) {
+        this.setState({ searchTerm: event.target.value });
+    }
+
+    handleSubmit(event) {
+        if (event.key === 'Enter' && this.state.searchTerm !== '') {
+            this.props.searchTerm(this.state.searchTerm);
+        }    
+    }
+
+    render() {
+        return (
+            <div id="search" className="Search">
+                <input
+                    type="search"
+                    name="title"
+                    onChange={this.handleSearch.bind(this)}
+                    onKeyUp={this.handleSubmit.bind(this)}
+                    placeholder="Search for a title" />
+            </div>
+        );
+    }
+}
