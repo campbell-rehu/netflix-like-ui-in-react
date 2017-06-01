530729109ccf1f89177b3fd14ce8abac41f2137f
campbell <campbellrehu@me.com>
2017-06-01 14:22:36 +1200

    Added Title logic

--- src/TitleList.js
+++ src/TitleList.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 export default class TitleList extends React.Component {
+    title() {
+        if (this.props.items.length !== 0) {
+            return <h1>{this.props.title}</h1>
+        }    
+    }
+    
     render() {
         return (
-            <ul>
-                {this.props.items.map((item) => { return <li key={item.id}>{item.title}</li>})}
-            </ul>
+            <div>
+                {this.title()}
+                <ul>
+                    {this.props.items.map((item) => { return <li key={item.id}>{this.props.tv ? item.name : item.title}</li>})}
+                    </ul>
+            </div>    
         )
     }
 }
