30f7588ebbbe11648fc767c73338c5b0839e44f1
campbell <campbellrehu@me.com>
2017-06-01 13:39:27 +1200

    Added TitleList component

---
+++ src/TitleList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class TitleList extends React.Component {
+    render() {
+        return (
+            <p>TitleList = {this.props.title}</p>
+        )
+    }
+}
