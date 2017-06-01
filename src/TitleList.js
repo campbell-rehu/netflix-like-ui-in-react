d36a6280d9b4eb34e1eef578ef4b0709499b5e68
campbell <campbellrehu@me.com>
2017-06-01 13:55:31 +1200

    Updated TitleList to reflect changes in App

--- src/TitleList.js
+++ src/TitleList.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default class TitleList extends React.Component {
     render() {
         return (
-            <p>TitleList = {this.props.title}</p>
+            <ul>
+                {this.props.items.map((item) => { return <li key={item.id}>{item.title}</li>})}
+            </ul>
         )
     }
 }
